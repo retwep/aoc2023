239de78f14aecf75daa13a74d0b6d4af8c96a6da
Nolan <nolan.letellier@gmail.com>
2023-12-06 22:00:39 -0700

    first tests pass

--- 3/parts.py
+++ 3/parts.py
@@ -1,81 +1,96 @@
+#!/usr/bin/env python
 import sys
+from typing import List, Set
 
-def get_digit(s:str):
-    if s[0] in "0123456789":
-        return int(s[0]),1
-    return None,1  # without see below, with 54338
-    names = ["zero","one","two","three","four","five","six","seven","eight","nine"]
-    for index,value in enumerate(names):
-        if s.startswith(value):
-            return index, 1  # len(value)  # 1 == 53389, len == 53407
-    return None,1
+digits = "0123456789"
 
-def code(l:str):
-    first = None
-    last = None
-    l = l.strip().lower()
-    # these 2 are in input.txt
-    # assert "twone" not in l
-    # assert "oneight" not in l
-    """ these are not in input.txt but trip the test cases
-    assert "zerone" not in l
-    assert "threeight" not in l
-    assert "nineight" not in l
-    """
-    i = 0
-    while i < len(l):
-        digit,skip = get_digit(l[i:])
-        i += skip
-        if digit is not None:
-            if first is None:
-                first = digit
-            last = digit
-    assert first is not None
-    result = 10*first+last
+def check_symbol(x):
+    for c in x:
+        if c not in digits and c != ".":
+            return True
+    return False
 
-    print(f"line:  {l}  {first=}, {last=}, {result=}")
-    return result
+def simple_scan(raw1, raw2) -> Set:
+    line1 = f".{raw1}." # very lazy edges
+    line2 = f".{raw2}."
+    parts = set()
+    number = ""
+    is_part = False
+    for i,c in enumerate(line1):
+        if c in digits:
+            number = number+c # may or may not be a part number
+            is_part |= check_symbol([line1[i-1], line1[i+1], line2[i-1], line2[i], line2[i+1]])
+        else:
+            if number:
+                if is_part:
+                    parts.add(int(number))
+                number = ""
+                is_part = False
+    if number:
+        if is_part:
+            parts.add(int(number))
+    return parts
+
+
+def scan_parts(line1, line2) -> Set:
+    found = simple_scan(line1, line2)
+    found = found.union(simple_scan(line2, line1))
+    return found
+
+def part_numbers(matrix) -> List:
+    parts = set()
+    blank = "." * len(matrix[0])
+    for i, line in enumerate(matrix):
+        assert len(line) == len(matrix[0])
+        found = scan_parts(line, matrix[i+1] if i+1 < len(matrix) else blank)
+        parts = parts.union(found)  # assumes part number 123 is only counted once no matter how many times it shows in the schematic
+    return sorted(list(parts))
+
 
 def test():
-    assert code("0") == 0
-    assert code("1") == 11
-    assert code("2") == 22
-    assert code("3") == 33
-    assert code("4") == 44
-    assert code("5") == 55
-    assert code("6") == 66
-    assert code("7") == 77
-    assert code("8") == 88
-    assert code("9") == 99
-    assert code("zero") == 0
-    assert code("one") == 11
-    assert code("two") == 22
-    assert code("three") == 33
-    assert code("four") == 44
-    assert code("five") == 55
-    assert code("six") == 66
-    assert code("seven") == 77
-    assert code("eight") == 88
-    assert code("nine") == 99
+    def check_part_numbers(raw, expected):
+        parts = part_numbers(raw)
+        assert parts == expected, f"{raw=}  {parts} != {expected}"
 
-    assert code("01") == 1
-    assert code("12") == 12
-    assert code("123") == 13
-    assert code("a1b") == 11
-    assert code("a1b2") == 12
-    assert code("3a1b2") == 32
+    # single line
+    check_part_numbers(["123"], [])
+    check_part_numbers([".123."], [])
+    check_part_numbers([".123"], [])
+    check_part_numbers(["123."], [])
+    check_part_numbers([".123!."], [123])
+    check_part_numbers([".#123"], [123])
+    check_part_numbers([".#123$"], [123])
+    check_part_numbers([".#123$123."], [123])
+    check_part_numbers([".#123..$123."], [123])
+    check_part_numbers(["123#456"], [123,456])
+    check_part_numbers(["123#%456.345"], [123,456])
+    check_part_numbers(["325.123#%456.345"], [123,456])
 
-    assert code("four1five") == 45
-    assert code("1six8") == 18
-    assert code("onetwo") == 12
-    assert code("nineight") == 98
-    assert code("nineeight") == 98
-    assert code("twone") == 21
-    assert code("twoone") == 21
-    assert code("oneabd") == 11
-    assert code("zxzeroneabd") == 1
-    assert code("zxzerooneabd") == 1
+    # above, below, diagonal
+    inputs = [(
+        ["123...456",
+         "^........"], [123]),
+        (["123...456",
+          ".^......."], [123]),
+        (["123...456",
+          "..^......"], [123]),
+        (["123...456",
+          "...^....."], [123]),
+        (["123...456",
+          "....^...."], []),
+        (["123...456",
+          ".....^..."], [456]),
+        (["123...456",
+          "......$.."], [456]),
+    ]
+    for i in inputs:
+        parts = part_numbers(i[0])
+        assert parts == i[1], f"found {parts}, expected {i[1]}"
+        swapped = [i[0][1], i[0][0]]
+        parts = part_numbers(swapped)
+        assert parts == i[1], f"(swapped) found {parts}, expected {i[1]}"
 
+    print("Test passed")
 
 def main():
     if len(sys.argv) < 2:
@@ -83,12 +98,10 @@ def main():
         return
     filename = sys.argv[1]
     with open(filename, "r") as f:
-        raw = f.readlines()
-    sum = 0
-    for line in raw:
-        value = code(line)
-        sum += value
-    print(f"{sum=}")
+        schematic = f.readlines()
+    parts = part_numbers(schematic)
+    part_sum = sum(parts)
+    print(f"{part_sum=}")
 
 if __name__=="__main__":
     main()
