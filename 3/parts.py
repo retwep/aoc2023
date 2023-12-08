9c6788ff34b1587b542bf3123ea41014e958d575
Nolan <nolan.letellier@gmail.com>
2023-12-07 23:21:31 -0700

    day3 part 1 528819 after do-over

--- 3/parts.py
+++ 3/parts.py
@@ -5,97 +5,124 @@
 # submission 1: previously got 332309 and web page says it was too low.
 
 import sys
-from typing import List, Set
+from typing import List, Tuple
 
 digits = "0123456789"
 
-def check_symbol(x):
+def check_symbol(x:str) -> str:
     for c in x:
         if c not in digits and c != ".":
             return c
     return ''
 
-def check_gear(tl,tc,tr, l, r, bl, bc, br):
-    # This identifies a gear, but doesn't give the numbers.
-    # need a solution that gives the numbers associated with a gear.
-    # Refactor to full OO with x/y coords and gear<-->partnumber references?
-    # or scan adjacent for part numbers and accumulate them?
+class Symbol:
+    def __init__(self, matrix:List[str], row:int, col:int, c:str):
+        self.matrix = matrix
+        self.row = row
+        self.col = col
+        self.symbol = c
+        self.numbers:List["PartNumber"] = list()
+        self.key = f"{row}x{col}"  # need this or not?
 
-    def zero_or_one_or_two(a,b,c):
-        if b in digits:
-            #  12.  .23  123
-            return 1
-        if a in digits and c in digits:
-            # 1.3
-            return 2
-        if a in digits or b in digits or c in digits:
-            # 1..  .2.  ..3
-            return 1
-        # ...
-        return 0 
-    count = zero_or_one_or_two(tl, tc, tr)
-    count += zero_or_one_or_two(bl, bc, br)
-    count += 1 if l in digits else 0
-    count += 1 if r in digits else 0
-    if count == 2:
-        return True # this is a gear, but what about the part numbers?
-    return False
-        
+    def is_gear(self):
+        return self.symbol == "*" and len(self.numbers) == 2
+    
+    def gear_power(self):
+        if not self.is_gear():
+            return None
+        return self.numbers[0].value * self.numbers[1].value
 
-def scan_parts(a, b, c) -> List:
-    above = f".{a}." # very lazy edges
-    here = f".{b}." # very lazy edges
-    below = f".{c}." # very lazy edges
-    parts = list()
-    number = ""
-    is_part = False
-    symbols = set()
-    for i,c in enumerate(here): 
-        if c == "*":
-            if check_gear(above[i-1], above[i], above[i+1], here[i-1], here[i+1], below[i-1], below[i], below[i+1]):
-                print(f"Found a gear! What do we do about it?\n{above}\n{here}\n{below}")
+    def add_number(self, number:"PartNumber"):
+        self.numbers.append(number)
+    
+class PartNumber:
+    def __init__(self, matrix:List[str], row:int, col:int, width:int):
+        self.matrix = matrix
+        self.row = row
+        self.col = col
+        self.width = width
+        self.symbols:List[Symbol] = list()
+        self.value = int(matrix[row][col:col+width])
+        self.key = f"{row}x{col}"  # leftmost digit
+    
+    def is_part(self):
+        return any(self.symbols)
 
-        if c in digits:
-            number = number+c # may or may not be a part number
-            cs = check_symbol([above[i-1], above[i], above[i+1], here[i-1], here[i+1], below[i-1], below[i], below[i+1]])
-            is_part |= bool(cs)
-            if cs:
-                symbols.add(cs) # something weird going on - look for numbers with more than one symbol (not a perfect check, but simple to start)
-        else:
-            if number:
-                if is_part:
-                    if len(symbols) > 1:
-                        print(f"Multi-symbol: {symbols=}")
-                    if int(number) in parts:
-                        print(f"A multi-part {number}")
-                    parts.append(int(number))
-                symbols = set()
-                number = ""
-                is_part = False
-    if number:
-        if is_part:
-            if len(symbols) > 1:
-                print(f"Multi-symbol: {symbols=}")
-            if int(number) in parts:
-                print(f"B multi-part {number}")
-            parts.append(int(number))
-    return parts
+    def scan_around_for_symbols(self, symbols):
+        left = self.col - 1
+        right = self.col + self.width
+        def generate_symbol(m:List[str], row:int, col:int):
+            nonlocal self
+            s = check_symbol(m[row][col])
+            if s:
+                symbol = Symbol(m, row, col, s)
+                self.symbols.append(symbol)
+                symbol.add_number(self)
+                return symbol
+            return None
+
+        # row above
+        for c in range(left, right+1):
+            generate_symbol(self.matrix, self.row-1, c)
+
+        # left/right
+        generate_symbol(self.matrix, self.row, left)
+        generate_symbol(self.matrix, self.row, right)
+
+        # row below
+        for c in range(left, right+1):
+            generate_symbol(self.matrix, self.row+1, c)
 
 
-def part_numbers(matrix) -> List:
-    parts = list()
-    blank = "." * len(matrix[0])
-    for i, line in enumerate(matrix):
-        assert len(line) == len(matrix[0])
-        found = scan_parts(blank if i == 0 else matrix[i-1], line, blank if i+1 == len(matrix) else matrix[i+1])
-        parts.extend(found)
-    return sorted(parts)
+    @staticmethod
+    def parse_number(matrix:List[str], row:int, col:int, numbers:List["PartNumber"], symbols:List[Symbol]) -> int:
+        width = 1
+        while matrix[row][col+width] in digits:
+            width += 1
+        number = PartNumber(matrix, row, col, width)
+        numbers.append(number)
 
+        number.scan_around_for_symbols(symbols)
+        return width
+
+def scan_the_matrix(matrix:List[str]) -> Tuple[List[PartNumber], List[Symbol]]:
+    part_numbers = list()
+    symbols = list()
+
+    lm0 = len(matrix[0])
+    row = 0
+    while row < len(matrix) - 1: # reduced because matrix was padded and we don't need to scan bottom line
+        col = 1 # skip padding
+        while col < lm0 - 1:
+            c = matrix[row][col]
+            if c in digits:
+                advance = PartNumber.parse_number(matrix, row, col, part_numbers, symbols)
+            else:
+                advance = 1
+            col += advance
+        row += 1
+    return (part_numbers, symbols)
+
+def pad_the_matrix(matrix:List[str]) -> List[str]:
+    # pad it once then ignore all boundary conditions. Yay Lazy.
+    lm0 = len(matrix[0])+2
+    blank = "." * lm0
+
+    padded = [blank]
+    for l in matrix:
+        padded.append(f".{l}.")
+    padded.append(blank)
+    return padded
 
 def test():
-    def check_part_numbers(raw, expected):
+    padded = pad_the_matrix(["x"])
+    assert padded == ["...", ".x.", "..."]
+
+    def check_part_numbers(raw:List[str], expected:List[int]):
         print(raw)
-        parts = part_numbers(raw)
+        matrix = pad_the_matrix(raw)
+        pn, s = scan_the_matrix(matrix)
+        parts = sorted([p.value for p in pn if p.is_part()])
         assert parts == expected, f"{raw=}  {parts} != {expected}"
 
     # single line
@@ -130,11 +157,9 @@ def test():
           "......$.."], [456]),
     ]
     for i in inputs:
-        parts = part_numbers(i[0])
-        assert parts == i[1], f"found {parts}, expected {i[1]}"
+        parts = check_part_numbers(i[0], i[1])
         swapped = [i[0][1], i[0][0]]
-        parts = part_numbers(swapped)
-        assert parts == i[1], f"(swapped) {swapped=}  found {parts}, expected {i[1]}"
+        parts = check_part_numbers(swapped, i[1])
 
     # above, below, diagonal
     inputs = [(
@@ -143,8 +168,7 @@ def test():
          "123...123"], [123,123,123,222,456,789])
     ]
     for i in inputs:
-        parts = part_numbers(i[0])
-        assert parts == i[1], f"found {parts}, expected {i[1]}"
+        check_part_numbers(i[0], i[1])
 
     # check for only single count if only single part
     for i in range(0,9):
@@ -152,11 +176,8 @@ def test():
         x[i] = "#"
         x[4] = "7"
         matrix = ["".join(x[0:3]),"".join(x[3:6]),"".join(x[6:9])]
-        parts = part_numbers(matrix)
         expected = [] if i == 4 else [7]
-        if parts != expected:
-            print(f"FAIL: {matrix=}, {parts=}, {expected=}")
-            assert False
+        check_part_numbers(matrix, expected)
 
     print("Test passed")
 
@@ -167,10 +188,14 @@ def main():
     filename = sys.argv[1]
     with open(filename, "r") as f:
         raw_lines = f.readlines()
-    schematic = [r.strip() for r in raw_lines]
-    parts = part_numbers(schematic)
+    unpadded = [r.strip() for r in raw_lines]
+    schematic = pad_the_matrix(unpadded)
+    numbers, symbols = scan_the_matrix(schematic)
+    parts = sorted([p.value for p in numbers if p.is_part()])
     part_sum = sum(parts)
     print(f"{part_sum=}")
+    gears = [s for s in symbols if s.is_gear()]
+    
 
 if __name__=="__main__":
     main()
