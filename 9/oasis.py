01a4d9bfe16eb92b9e1d8075b42a6191cb0242df
Nolan <nolan.letellier@gmail.com>
2023-12-08 23:27:07 -0700

    Day 9 part 2 passes with 1026

--- 9/oasis.py
+++ 9/oasis.py
@@ -8,9 +8,8 @@ def diffs(values:List[int]) -> List[int]:
     for i in range(len(values)-1):
         diff.append(values[i+1]-values[i])
     return diff
-        
 
-def interpolate(values:List[int]) -> List[int]:
+def make_layer_diffs(values:List[int]) -> List[int]:
     layers = list()
     layers.append(values.copy())  # first layer is the input
     while True:
@@ -19,10 +18,24 @@ def interpolate(values:List[int]) -> List[int]:
         assert next
         if not any(next):
             break
+    return layers
+
+
+def interpolate(values:List[int]) -> List[int]:
+    layers = make_layer_diffs(values)
 
     for i in range(1,len(layers)):
         new = layers[-i][-1] + layers[-(i+1)][-1]
         layers[-(i+1)].append(new)
+
+    return layers[0]
+
+def interpolate_back(values:List[int]) -> List[int]:
+    layers = make_layer_diffs(values)
+
+    for i in range(1,len(layers)):
+        new = layers[-(i+1)][0] - layers[-i][0]
+        layers[-(i+1)].insert(0,new)
 
     return layers[0]
 
@@ -31,6 +44,10 @@ def test():
     assert interpolate([1, 1]) == [1, 1, 1]
     assert interpolate([1, 2, 3]) == [1,2,3,4]
     assert interpolate([1,2,4,7]) == [1,2,4,7,11]
+
+    assert interpolate_back([1, 1]) == [1, 1, 1]
+    assert interpolate_back([1, 2, 3]) == [0,1,2,3]
+    assert interpolate_back([1,2,4,7]) == [1,1,2,4,7]
 
     print("Test passed")
 
@@ -53,12 +70,19 @@ def main():
 
     readings = parse_readings(lines)
     sum = 0
+    backsum = 0
     for reading in readings:
         interpolated = interpolate(reading)
         if len(reading) < 10:
             print(f"{interpolated=}")
         sum += interpolated[-1]
+        back = interpolate_back(reading)
+        if len(reading) < 10:
+            print(f"{back=}")
+        backsum += back[0]
+
     print(f"Sum of interpolations {sum}")
+    print(f"Sum of back interpolations {backsum}")
 
 
 
