2224f4962dadc8934c9b05def52178d608858fd2
Nolan <nolan.letellier@gmail.com>
2023-12-18 22:05:30 -0700

    day 19 skeleton

---
+++ 19/flow.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+import sys
+from typing import Dict, List, Tuple
+
+
+
+def test():
+    print("Test passed")
+
+def main():
+    if len(sys.argv) < 2:
+        test()
+        return
+    filename = sys.argv[1]
+    with open(filename, "r") as f:
+        raw_lines = f.readlines()
+    lines = [r.strip() for r in raw_lines]
+
+if __name__=="__main__":
+    main()
